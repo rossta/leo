f94eb99530f32dc1ba32deb1996d16829c91cf77
Ross Kaffenberger <ross@weplay.com>
2009-10-30 10:25:11 -0400

    First passing jspec unit test

--- spec/artcart.spec.js
+++ spec/artcart.spec.js
@@ -1,16 +1,20 @@
 JSpec.describe('ArtCart', function() {
   before_each(function(){
     canvas = $(fixture("canvas"));
-    $("body").append(canvas);
+    brush_picker = $(fixture("brush_picker"));
+    $("body").
+      append(canvas).
+      append(brush_picker);
   });
   after_each(function() {
     canvas.remove();
+    brush_picker.remove();
   });
   
   describe('init', function() {
     it('should create a painter', function() {
       ArtCart.init();
-      ArtCart.painter.should(be_an_instance_of(String));
+      ArtCart.painter.should.be_an_instance_of(ArtCart.Painter);
     });
   });
 });
