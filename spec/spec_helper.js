e4648e4916e5899d4b5e508c8b92d680ad4cd6b0
Ross Kaffenberger <ross@confucius.local>
2009-11-27 22:06:05 -0500

    Bump screw-unit

--- spec/spec_helper.js
+++ spec/spec_helper.js
@@ -1,18 +1,3 @@
-Screw.Matchers["include"] = {
-  match: function(expected, actual) {
-    for(var i = 0; i < actual.length; i++) {
-      if (Screw.Matchers["equal"]["match"](expected, actual[i])) {
-        return true;
-      }
-    }
-    return false;
-  },
-
-  failure_message: function(expected, actual, not) {
-    return 'expected ' + $.print(actual) + (not ? ' not' : '') + ' to contain ' + $.print(expected);
-  }
-};
-
 var fixture = function(html) {
   $fixture = $("#fixture");
   if (!$fixture.length) $fixture = $("<div id='fixture'></div>").appendTo('body');
