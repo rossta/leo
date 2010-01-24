3af5ee04a1594b6903bba44909310877e3db62c1
rosskaff <rosskaff@gmail.com>
2010-01-23 19:00:19 -0500

    Update spec helper js and requires for running specs

--- spec/spec_helper.js
+++ spec/spec_helper.js
@@ -1,43 +1,44 @@
-var fixture = function(html) {
-  $fixture = $("#fixture");
-  if (!$fixture.length) $fixture = $("<div id='fixture'></div>").appendTo('body');
-  $scenario = $("<div class='scenario'></div>");
-  $fixture.append($scenario.append(html));
-},
-cleanFixtures = function() {
-      $("#fixture").remove();
-},
-mockEvent = function(x, y) {
-  var mockEvent = Screw.Matchers.mock(Event);
-  if (x) Screw.Matchers.stub(mockEvent, "clientX").and_set_to(x);
-  if (y) Screw.Matchers.stub(mockEvent, "clientY").and_set_to(y);
-  return mockEvent;
-},
-FileLoader = function() {
-  this.xhr = function() {
-    return this.ieXhr() || new XMLHttpRequest();
-  };
-  this.ieXhr = function() {
-    function object(str) {
-      try { return new ActiveXObject(str); } catch(e) {}
-    }
-    return object('Msxml2.XMLHTTP.6.0') ||
-      object('Msxml2.XMLHTTP.3.0') ||
-      object('Msxml2.XMLHTTP') ||
-      object('Microsoft.XMLHTTP');
-  };
-  this.tryLoading = function(file) {
-    try { return this.load(file); }
-    catch (e) {};
-  };
-  this.load = function(file) {
-    var request = this.xhr();
-    request.open('GET', file, false);
-    request.send(null);
-    if (request.readyState == 4 && (request.status == 0 || request.status.toString().charAt(0) == 2)) return request.responseText;
-  };
-},
-load = function(file) {
-  if (!this.fileLoader) this.fileLoader = new FileLoader();
-  return this.fileLoader.tryLoading(file);
+require = function(src) {
+  var element = document.createElement("script");
+  element.setAttribute("type", "text/javascript");
+  element.setAttribute("src", src);
+  document.getElementsByTagName("head")[0].appendChild(element);
 };
+
+require('support/spec.js');
+require('support/file_loader.js');
+
+// Test framework
+require("../vendor/screw-unit/lib/screw.builder.js");
+require("../vendor/screw-unit/lib/screw.matchers.js");
+require("../vendor/screw-unit/lib/screw.events.js");
+require("../vendor/screw-unit/lib/screw.behaviors.js");
+require("../vendor/screw-unit/lib/jquery.fn.js");
+require("../vendor/screw-unit/lib/jquery.print.js");
+require("../vendor/smoke/lib/smoke.core.js");
+require("../vendor/smoke/lib/smoke.mock.js");
+require("../vendor/smoke/lib/smoke.stub.js");
+require("../vendor/smoke/plugins/screw.mocking.js");
+
+// Vendor
+require("../vendor/base/base.js");
+
+// Under test
+require("../lib/artcart.js");
+require("../lib/artcart.observable.js");
+require("../lib/artcart.event_handling.js");
+require("../lib/artcart.canvas.js");
+require("../lib/artcart.sandbox.js");
+require("../lib/artcart.logger.js");
+require("../lib/artcart.position.js");
+require("../lib/artcart.drawing.js");
+require("../lib/artcart.painter.js");
+require("../lib/artcart.brush.js");
+require("../lib/artcart.drawing.js");
+
+// Specs
+require("artcart.drawing_spec.js");
+require("artcart.painter_spec.js");
+require("artcart.core_spec.js");
+require("artcart.position_spec.js");
+require("artcart.brush_spec.js");
