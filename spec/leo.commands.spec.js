107187d700623847cbb14b3d38b528efc0e1c4b5
rosskaff <rosskaff@gmail.com>
2010-02-06 14:26:06 -0800

    Rework ellipse node handling and command stack execute with undo

--- spec/leo.commands.spec.js
+++ spec/leo.commands.spec.js
@@ -1,9 +1,9 @@
 Screw.Unit(function() {
 
-  describe("Leo.EllipseNodeCommand", function() {
+  describe("Leo.DrawEllipseCommand", function() {
 
     it("should set view", function() {
-      var node = mock(Leo.Node), view = mock(Leo.View), command = new Leo.EllipseNodeCommand(view);
+      var node = mock(Leo.Node), view = mock(Leo.View), command = new Leo.DrawEllipseCommand(view);
       expect(command.view).to(equal, view);
     });
     
--- spec/leo.spec.js
+++ spec/leo.spec.js
@@ -29,22 +29,18 @@ Screw.Unit(function() {
     describe('self.init', function() {
 
       it("should create a new Leo instance", function() {
-        var leo = Leo.init();
-        expect(leo).to(be_instance_of, Leo);
+        expect($leo).to(be_instance_of, Leo);
       });
 
       it("should append a canvas to the element", function() {
-        Leo.init();
         expect($("#leo")).to(contain_selector, 'canvas');
       });
 
       it("should create a new Leo.View", function() {
-        var leo = Leo.init();
         expect(leo.module('view')).to(be_instance_of, Leo.View);
       });
 
       it("should create a new Leo.Toolbar", function() {
-        var leo = Leo.init();
         expect(leo.module('toolbar')).to(be_instance_of, Leo.Toolbar);
       });
 
